13eec5ab58549fe439963536a81d2c11de7569c8
kilokilo97429@gmail.com <kilokilo97429@gmail.com>
2019-12-18 16:02:41 +0100

    ajout de src et pom.xml

---
+++ src/main/java/julien/testProjet/App.java
@@ -0,0 +1,24 @@
+package julien.testProjet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public int multiply (int x, int y) {
+		
+		return	x*y;
+		
+		
+	}
+	
+	
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
